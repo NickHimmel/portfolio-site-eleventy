f26d7535d1d5e2f9067b8b0af4672fbb2c836991
NickHimmel <himmel.nick@gmail.com>
2020-04-19 18:01:18 -0400

    add mobile styles

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,9 @@ module.exports = function (eleventyConfig) {
     const current = posts.findIndex(post => post.data.title === title);
     const previous = posts[current===0?posts.length-1:current-1];
     const next = posts[current===posts.length-1?0:current+1];
-    return `<a href=${previous.url}><span class="footer-link">Previous project:</span> ${previous.data.title}</a><a href=${next.url}><span class="footer-link">Next project:</span> ${next.data.title}</a>`;
+    const previousLink = `<a href=${previous.url} class="footer-link"><span class="footer-desktop"><span class="link-light">Previous project:</span> ${previous.data.title}</span><span class="footer-mobile">←</span></a>`;
+    const nextLink = `<a href=${next.url} class="footer-link"><span class="footer-desktop"><span class="link-light">Next project:</span> ${next.data.title}</span><span class="footer-mobile">→</span></a>`
+    return previousLink + nextLink;
   });
 
   eleventyConfig.addPassthroughCopy("css");
